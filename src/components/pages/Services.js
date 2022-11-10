983181f5476f0f67eadd89eb5c72215f32d377e3
yuripelichek <57309738+yuripelichek@users.noreply.github.com>
2022-11-10 17:49:54 -0300

    Inclusão de texto

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,5 +2,10 @@ import React from 'react';
 import '../../App.css';
 
 export default function Services() {
-  return <h1 className='services'>pagina teste 3</h1>;
+  return <h1 className='servicesText' >Bem vindos, condôminos!!!
+  
+  Nosso site está em desenvolvimento, temos como objetivo informar sobre o condomínio como um todo, e temos disponível nosso canal de comunicação, 
+  para podermos ouvir suas críticas e sugestões em relação ao condomínio. 
+  No momento estamos aliando as informações de coleta dos recicláveis e coletando as sugestões, ao final do ano teremos todas as informações referente a horáios de piscina, 
+  funcionamento de encomenda, aluguel dos salões, etc.</h1>;
 }
